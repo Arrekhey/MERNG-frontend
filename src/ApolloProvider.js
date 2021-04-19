275c4ffee17e5cce6e792d7002c55e0a4a591887
Rama Krishna Pilla <pillaramakrishna77@yahoo.com>
2021-04-19 09:58:14 +0530

    URI path changed

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -7,7 +7,7 @@ import { ApolloProvider } from '@apollo/react-hooks';
 import { setContext } from 'apollo-link-context';
 
 const httpLink = createHttpLink({
-  uri: ''
+  uri: 'https://thawing-chamber-35563.herokuapp.com/'
 });
 
 const authLink = setContext(() => {
